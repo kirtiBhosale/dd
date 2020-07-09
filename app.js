7fd312af05be83cc6fbc117e79a5a30dd8ca741d
kirtiBhosale <61185694+kirtiBhosale@users.noreply.github.com>
2020-07-09 09:42:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 express.static('public')
 
 app.get('/', function(req, res){
-  res.sendFile('F:/2700439/NodeJs/AppPages' + '/MyPage.html');
+  res.sendFile('../AppPages' + '/MyPage.html');
 });
 app.post('/', function(req, res) {
     var item = req.body.userSearchInput;
